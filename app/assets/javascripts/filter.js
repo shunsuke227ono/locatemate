6f7e92cd636b6c36c6e4df35516473159c43f92b
onoshunsuke <shunsuke.ono@outlook.jp>
2014-08-08 15:57:07 +0930

    html,css,js modify

---
+++ app/assets/javascripts/filter.js
@@ -0,0 +1,11 @@
+//this is js for filters.
+
+function showFriendRange(newValue)
+{
+	document.querySelector("#friend-range").value=newValue;
+}
+
+function showTimeRange(newValue)
+{
+	document.getElementById("#time-range").innerHTML=newValue;
+}
